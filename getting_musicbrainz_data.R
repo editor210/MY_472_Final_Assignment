9fc9bb8c6a0fbc5808c48503ec2bdf05bc28f6ab
editor210 <edimilson.torres@gmail.com>
2023-12-29 20:25:32 +0000

    All plots finished and running

--- getting_musicbrainz_data.R
+++ getting_musicbrainz_data.R
@@ -43,11 +43,11 @@ for (artist in artist_info_ranking$artist_spotify){
   
   #selecting the first row as the correct result
   new_row <- results_table[1,]
-  new_row <- cbind(artist = artist, new_row)
+  new_row <- cbind(artist_spotify = artist, new_row)
   mbdata <- rbind(mbdata, new_row)
   
   #Saving all other results in case there is mismatch
-  result_rows <- cbind(artist = artist, results_table)
+  result_rows <- cbind(artist_spotify = artist, results_table)
   all_results <- rbind(all_results, result_rows)
 }
 
@@ -69,5 +69,17 @@ the_drifters <- the_drifters[2,]
 drifters_index <- which(mbdata$artist == "The Drifters")
 # Update the corresponding row in mbdata with the corrected information
 mbdata[drifters_index, ] <- the_drifters
+
+
+#Treating the Begin and End datas to make it in uniform format
+#If there is only the year and month, the day 01 is assigned
+#If there is only the year, the first of july of the year is assigned
+
+mbdata$Begin <- ifelse(nchar(mbdata$Begin) == 4, paste0(mbdata$Begin, "-07-01"), mbdata$Begin)
+mbdata$Begin <- ifelse(nchar(mbdata$Begin) == 7, paste0(mbdata$Begin, "-01"), mbdata$Begin)
+
+mbdata$End <- ifelse(is.na(mbdata$End), "", mbdata$End)
+mbdata$End <- ifelse(nchar(mbdata$End) == 4, paste0(mbdata$End, "-07-01"), mbdata$End)
+mbdata$End <- ifelse(nchar(mbdata$End) == 7, paste0(mbdata$End, "-01"), mbdata$End)
 
 saveRDS(mbdata, ".\\data\\mbdata.rds")
